710da872dbc6617e77258aac85ae618364b22442
Amokakeng <atthasit.p@serieadigiventure.com>
2026-07-24 10:17:52 +0700

    Add files via upload

--- update.js
+++ update.js
@@ -2,6 +2,6 @@
 <!-- Serie A Digiventure Co., Ltd. — LEO Seeding Matcher update manifest (served as .js to bypass Imunify WebShield) -->
 <gupdate xmlns='http://www.google.com/update2/response' protocol='2.0'>
   <app appid='hhejpehnifhoniafhhkcjffapaokafin'>
-    <updatecheck codebase='https://serieadigiventure.com/seeding/leo-capture/leo-seeding-matcher.crx.js' version='0.2.1' />
+    <updatecheck codebase='https://serieadigiventure.com/seeding/leo-capture/leo-seeding-matcher.crx.js' version='0.3.0' />
   </app>
 </gupdate>
